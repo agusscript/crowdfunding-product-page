6a4344ab2833dd7233e196cbe396537b2e443288
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-03-08 16:10:21 -0300

    Add manageModalRewardsState function to open reward inputs

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function manageMobileMenu() {
   overLay.classList.toggle("show");
 }
 
-function manageBookmarkSectionState () {
+function manageBookmarkSectionState() {
   const defaultBookmarkText = document.querySelector(".default-bookmark-text");
   const activeBookmarkText = document.querySelector(".active-bookmark-text");
   const svgBookmarkButton = document.querySelector(".svg-bookmark");
@@ -25,5 +25,43 @@ function manageBookmarkSectionState () {
   svgBookmarkButton.querySelector("circle").classList.toggle("circle-active");
 }
 
+const radioButtons = document.querySelectorAll("input[type='radio']");
+const pledgeTitles = document.querySelectorAll(".pledge-title");
+const modalsReward = document.querySelectorAll(".modal-reward");
+const pledgeContainers = document.querySelectorAll(".pledge-container");
+
+function manageModalRewardsState() {
+  pledgeTitles.forEach(function (element, index) {
+    element.onclick = function () {
+      radioButtons[index].checked = true;
+
+      if (radioButtons[0].checked === true) {
+        modalsReward[0].classList.add("active-pledge");
+        pledgeContainers[0].classList.add("show");
+      } else {
+        modalsReward[0].classList.remove("active-pledge");
+        pledgeContainers[0].classList.remove("show");
+      }
+
+      if (radioButtons[1].checked === true) {
+        modalsReward[1].classList.add("active-pledge");
+        pledgeContainers[1].classList.add("show");
+      } else {
+        modalsReward[1].classList.remove("active-pledge");
+        pledgeContainers[1].classList.remove("show");
+      }
+
+      if (radioButtons[2].checked === true) {
+        modalsReward[2].classList.add("active-pledge");
+        pledgeContainers[2].classList.add("show");
+      } else {
+        modalsReward[2].classList.remove("active-pledge");
+        pledgeContainers[2].classList.remove("show");
+      }
+    };
+  });
+}
+
 bookmarkButton.onclick = manageBookmarkSectionState;
 menuButton.onclick = manageMobileMenu;
+manageModalRewardsState();
