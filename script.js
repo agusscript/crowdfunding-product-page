443fd9a686738ccd132279c51f3f4ff46d27449e
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-03-09 15:13:05 -0300

    Add function to manage styles when radio buttons are clicked

--- script.js
+++ script.js
@@ -30,7 +30,37 @@ const pledgeTitles = document.querySelectorAll(".pledge-title");
 const modalsReward = document.querySelectorAll(".modal-reward");
 const pledgeContainers = document.querySelectorAll(".pledge-container");
 
-function manageModalRewardsState() {
+function manageRadioButtonsModalState() {
+  radioButtons.forEach(function (element, index) {
+    element.onclick = function () {
+      radioButtons[index].checked = true;
+
+      if (radioButtons[0].checked === true) {
+        modalsReward[0].classList.add("active-pledge");
+        pledgeContainers[0].classList.add("show");
+      } else {
+        modalsReward[0].classList.remove("active-pledge");
+        pledgeContainers[0].classList.remove("show");
+      }
+
+      if (radioButtons[1].checked === true) {
+        modalsReward[1].classList.add("active-pledge");
+        pledgeContainers[1].classList.add("show");
+      } else {
+        modalsReward[1].classList.remove("active-pledge");
+        pledgeContainers[1].classList.remove("show");
+      }
+
+      if (radioButtons[2].checked === true) {
+        modalsReward[2].classList.add("active-pledge");
+        pledgeContainers[2].classList.add("show");
+      } else {
+        modalsReward[2].classList.remove("active-pledge");
+        pledgeContainers[2].classList.remove("show");
+      }
+    };
+  });
+
   pledgeTitles.forEach(function (element, index) {
     element.onclick = function () {
       radioButtons[index].checked = true;
@@ -64,4 +94,4 @@ function manageModalRewardsState() {
 
 bookmarkButton.onclick = manageBookmarkSectionState;
 menuButton.onclick = manageMobileMenu;
-manageModalRewardsState();
+manageRadioButtonsModalState();
